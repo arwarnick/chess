3bcbe9839c79ef995bd515dc452f704e75d3d069
Austin Warnick <arwarnick@gmail.com>
2024-08-02 20:42:11 -0600

    modified Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,25 @@
-import chess.*;
+import client.ServerFacade;
+import ui.PreloginUI;
+import ui.PostloginUI;
+import result.LoginResult;
 
 public class Main {
+    private static final String SERVER_URL = "http://localhost:8080";
+
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        ServerFacade server = new ServerFacade(SERVER_URL);
+        runApplication(server);
+    }
+
+    private static void runApplication(ServerFacade server) {
+        PreloginUI preloginUI = new PreloginUI(server);
+        LoginResult loginResult = preloginUI.run();
+
+        if (loginResult != null) {
+            PostloginUI postloginUI = new PostloginUI(server, loginResult.authToken());
+            postloginUI.run();
+        }
+
+        System.out.println("Thank you for using the Chess Client. Goodbye!");
     }
 }
